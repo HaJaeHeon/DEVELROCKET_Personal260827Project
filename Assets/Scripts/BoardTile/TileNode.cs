b79f2cae9e094ea2e30834121425eb14479e1cb0
HaJaeHeon <gkhen@naver.com>
2026-09-01 18:00:18 +0900

    [Feat] Map Object
    [Feat] Map Object

--- Assets/Scripts/BoardTile/TileNode.cs
+++ Assets/Scripts/BoardTile/TileNode.cs
@@ -17,10 +17,7 @@ public class TileNode : MonoBehaviour
     // 한 줄 내 일반 타일의 크기를 일단 9로 잡음 (일반 타일 8칸 + 특수타일 1칸)
     public void SetReward()
     {
-        //Debug.Log($"{gameObject.name} :인덱스 {tileIndex}");
         reward = (int)((tileIndex % 9 * 0.1f + 1f) * data.tileReward);
-        //Debug.Log($"{gameObject.name} : 타일 리워드 {data.tileReward}");
-        //Debug.Log($"{gameObject.name} : 리워드 {reward}");
     }
 
     public void SetTileType()
